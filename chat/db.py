50cac82e99f0058df7832a0e41d4f5f41fdfc60b
Eric Korber <erickorber1994@gmail.com>
2026-04-02 21:56:24 -0400

    Removed unnecessary seed functionality for database.

--- chat/db.py
+++ chat/db.py
@@ -12,6 +12,4 @@ def get_db():
         _db = _client.get_default_database()
         _db.messages.create_index("lead_id")
 
-        from content.seed import seed_campaigns
-        seed_campaigns(_db)
     return _db
